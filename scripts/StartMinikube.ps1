d38622a15332f296201ce4841af8fcbef859235b
stefaanc <stefaan.coussement@gmail.com>
2023-02-17 11:46:25 +0000

    Update StartMinikube.ps1

--- scripts/StartMinikube.ps1
+++ scripts/StartMinikube.ps1
@@ -1,14 +1,15 @@
 #
-# Start minikube
+# the following will most probably not properly run as a script - it needs to wait for completion after some steps
+# I copy & execute the following line-by-line into a powershell teminal and verify each step manually
+#
+
+#
+# start minikube
 #
 Get-NetAdapter
 New-VMSwitch -Name "External Switch" -NetAdapterName "Ethernet"
-
-#minikube start --vm-driver hyperv --hyperv-virtual-switch "External Switch"
-#minikube mount "${HOME}:/host"
-### or
 minikube start --vm-driver hyperv --hyperv-virtual-switch "External Switch" --mount --mount-string "${HOME}:/host"
-
+# test
 minikube ssh
 
 #
@@ -16,12 +17,14 @@ minikube ssh
 #
 minikube addons enable metrics-server
 minikube addons enable dashboard
+# test
 minikube dashboard
 
 #
 # enable ingress
 #
 minikube addons enable ingress
+kubectl apply -f $HOME\projects\minikube-jenkins-blue-ocean\manifests\kubernetes-dashboard
 
 #
 # enable ingress-dns
@@ -29,6 +32,5 @@ minikube addons enable ingress
 minikube addons enable ingress-dns
 Get-DnsClientNrptRule | Where-Object {$_.Namespace -eq '.minikube'} | Remove-DnsClientNrptRule -Force
 Add-DnsClientNrptRule -Namespace ".minikube" -NameServers "$(minikube ip)"
-
-# test with dashboard
-kubectl apply -f $HOME\projects\minikube-jenkins-blue-ocean\manifests\kubernetes-dashboard
+# test
+Start-Process "http://dashboard.minikube"
